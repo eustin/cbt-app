7d0966050c9978c8f8428ac358e17636466e6ae1
Justin Evans <eustin.jvans@gmail.com>
2022-06-05 14:24:43 +1000

    create FlatList to display a list of jrounal entries

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const journalEntries = [
+  { 
+    id: 1, 
+    situation: "I'm worried about my presentation that I need to give tomorrow.",
+    threatCognition: "No one will like my presentation.",
+    emotions: "Worry, anxiety, fear",
+    behaviours: "Phone in sick. Try to avoid giving presentation.",
+  },
+];
+
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Welcome back!</Text>
+      <FlatList 
+        data={journalEntries}
+        renderItem={({ item }) => {
+          console.log(item);
+          return (
+            <View>
+              <Text>{item.situation}</Text>
+            </View>
+          );
+        }}
+        keyExtractor={journalEntry => journalEntry.id}
+      />
     </View>
   );
 };
