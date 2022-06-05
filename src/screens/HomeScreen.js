744e088fbc9bbeb3a3f3adc2ee41f4590525024e
Justin Evans <eustin.jvans@gmail.com>
2022-06-05 15:57:11 +1000

    navigate to ViewEntryScreen on tapping JournalEntry component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,38 +1,50 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import JournalEntry from "../components/JournalEntry";
 
 const journalEntries = [
   {
     id: 1,
-    situation:
-      "I'm worried about my presentation that I need to give.",
+    situation: "I'm worried about my presentation that I need to give.",
     threatCognition: "No one will like my presentation.",
     emotions: "Worry, anxiety, fear",
     behaviours: "Phone in sick. Try to avoid giving presentation.",
     alternativeHypotheses: "People actually want to hear what I have to say.",
-    whatActuallyHappened: "A colleague told me that it was the best presentation he's seen at work.",
+    whatActuallyHappened:
+      "A colleague told me that it was the best presentation he's seen at work.",
   },
   {
     id: 2,
-    situation:
-      "I feel stuck in my job and my career.",
+    situation: "I feel stuck in my job and my career.",
     threatCognition: "I'm not good enough and no one will hire me.",
     emotions: "Fear, depressed, sad",
     behaviours: "Avoid looking for job. Socially withdraw.",
     alternativeHypotheses: "I am highly employable.",
-    whatActuallyHappened: "I applied to several roles and was invited to an interview!",
+    whatActuallyHappened:
+      "I applied to several roles and was invited to an interview!",
   },
 ];
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.title}>Welcome back!</Text>
       <FlatList
         data={journalEntries}
         renderItem={({ item }) => {
-          return <JournalEntry title={item.situation} />;
+          return (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("View Entry", item)}
+            >
+              <JournalEntry title={item.situation} />
+            </TouchableOpacity>
+          );
         }}
         keyExtractor={(journalEntry) => journalEntry.id}
       />
@@ -45,8 +57,8 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     paddingHorizontal: 10,
-    marginVertical: 20,
-  }
+    marginVertical: 10,
+  },
 });
 
 export default HomeScreen;
