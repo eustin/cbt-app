4ae89d34e7caa2ac84d0f0cabfa767dc7d5fd14c
Justin Evans <eustin.jvans@gmail.com>
2022-06-05 14:57:53 +1000

    use JournalEntry component in HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,37 +1,52 @@
-import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import JournalEntry from "../components/JournalEntry";
 
 const journalEntries = [
-  { 
-    id: 1, 
-    situation: "I'm worried about my presentation that I need to give tomorrow.",
+  {
+    id: 1,
+    situation:
+      "I'm worried about my presentation that I need to give.",
     threatCognition: "No one will like my presentation.",
     emotions: "Worry, anxiety, fear",
     behaviours: "Phone in sick. Try to avoid giving presentation.",
+    alternativeHypotheses: "People actually want to hear what I have to say.",
+    whatActuallyHappened: "A colleague told me that it was the best presentation he's seen at work.",
+  },
+  {
+    id: 2,
+    situation:
+      "I feel stuck in my job and my career.",
+    threatCognition: "I'm not good enough and no one will hire me.",
+    emotions: "Fear, depressed, sad",
+    behaviours: "Avoid looking for job. Socially withdraw.",
+    alternativeHypotheses: "I am highly employable.",
+    whatActuallyHappened: "I applied to several roles and was invited to an interview!",
   },
 ];
-
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text>Welcome back!</Text>
-      <FlatList 
+      <Text style={styles.title}>Welcome back!</Text>
+      <FlatList
         data={journalEntries}
         renderItem={({ item }) => {
-          console.log(item);
-          return (
-            <View>
-              <Text>{item.situation}</Text>
-            </View>
-          );
+          return <JournalEntry title={item.situation} />;
         }}
-        keyExtractor={journalEntry => journalEntry.id}
+        keyExtractor={(journalEntry) => journalEntry.id}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingHorizontal: 10,
+    marginVertical: 20,
+  }
+});
 
 export default HomeScreen;
