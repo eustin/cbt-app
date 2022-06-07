c0ad8be7eb6247e6764a9185a4df2a580eb81771
Justin Evans <eustin.jvans@gmail.com>
2022-06-07 10:03:40 +1000

    Wrap ViewEntryScreen contents in ScrollView

--- src/screens/ViewEntryScreen.js
+++ src/screens/ViewEntryScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 const ViewEntryScreen = ({ route }) => {
   const {
@@ -12,35 +12,38 @@ const ViewEntryScreen = ({ route }) => {
   } = route.params;
   return (
     <View style={styles.container}>
-      <Text style={styles.question}>What is the situation?</Text>
-      <Text style={styles.answer}>{situation}</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={styles.question}>What is the situation?</Text>
+        <Text style={styles.answer}>{situation}</Text>
 
-      <Text style={styles.question}>
-        What thoughts are you having about it?
-      </Text>
-      <Text style={styles.answer}>{threatCognition}</Text>
+        <Text style={styles.question}>
+          What thoughts are you having about it?
+        </Text>
+        <Text style={styles.answer}>{threatCognition}</Text>
 
-      <Text style={styles.question}>What emotions are you experiencing?</Text>
-      <Text style={styles.answer}>{emotions}</Text>
+        <Text style={styles.question}>What emotions are you experiencing?</Text>
+        <Text style={styles.answer}>{emotions}</Text>
 
-      <Text style={styles.question}>
-        How are you behaving in response to the situation?
-      </Text>
-      <Text style={styles.answer}>{behaviours}</Text>
+        <Text style={styles.question}>
+          How are you behaving in response to the situation?
+        </Text>
+        <Text style={styles.answer}>{behaviours}</Text>
 
-      <Text style={styles.question}>
-        What are some positive alternative hypotheses?
-      </Text>
-      <Text style={styles.answer}>{alternativeHypotheses}</Text>
+        <Text style={styles.question}>
+          What are some positive alternative hypotheses?
+        </Text>
+        <Text style={styles.answer}>{alternativeHypotheses}</Text>
 
-      <Text style={styles.question}>What actually happened?</Text>
-      <Text style={styles.answer}>{whatActuallyHappened}</Text>
+        <Text style={styles.question}>What actually happened?</Text>
+        <Text style={styles.answer}>{whatActuallyHappened}</Text>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     marginTop: 10,
     marginHorizontal: 15,
   },
@@ -48,12 +51,11 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     marginBottom: 10,
-  }, 
+  },
   answer: {
     fontSize: 18,
     marginBottom: 15,
-
-  }
+  },
 });
 
 export default ViewEntryScreen;
